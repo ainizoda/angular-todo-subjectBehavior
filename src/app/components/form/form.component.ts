51e8503108824cd0763a80bda0d25c5d47dc9af8
ainizoda <ainizodam@gmail.com>
2022-07-26 10:48:46 +0500

    add validation to form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormControlState,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 import { Todo } from '../todo/todo.models';
 import { TodoService } from '../todo/todo.service';
@@ -17,26 +23,51 @@ export interface TodoForm {
   styleUrls: ['./form.component.scss'],
   templateUrl: `./form.component.html`,
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
+  public hasError: boolean = false;
+
   public todoForm: FormGroup = new FormGroup({
-    title: new FormControl('', Validators.required),
+    title: new FormControl('', [
+      Validators.required,
+      Validators.min(8),
+      Validators.max(25),
+    ]),
     info: new FormGroup({
-      description: new FormControl('', Validators.required),
-      author: new FormControl('', Validators.required),
+      description: new FormControl('', [
+        Validators.required,
+        Validators.min(8),
+      ]),
+      author: new FormControl('', [Validators.required, Validators.min(8)]),
     }),
   });
 
-  ngOnInit() {
-    this.todoForm.valueChanges.subscribe(() => {
-      this.shouldButtonBeDisabled = this.todoForm.invalid;
-    });
-  }
-
-  public shouldButtonBeDisabled: boolean = this.todoForm.invalid;
+  public todoControls: any = this.todoForm.controls;
 
   constructor(private _todoService: TodoService) {}
 
+  validateControl(control: AbstractControl | null) {
+    return control?.invalid && this.hasError;
+  }
+
+  logError(control: AbstractControl | null) {
+    let errorTexts: any = {
+      required: 'This field is required',
+      minlength: 'This field must be at least 8 characters long',
+    };
+
+    if (control?.errors) {
+      return errorTexts[Object.keys(control?.errors)[0]];
+    }
+  }
+
   handleSubmit() {
+    if (this.todoForm.invalid) {
+      this.hasError = true;
+      return;
+    }
+
+    this.hasError = false;
+
     const todo: Todo = new Todo(this.todoForm.value);
 
     this._todoService.addTodo(todo);
